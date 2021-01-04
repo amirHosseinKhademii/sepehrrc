45d69f346d247f1e4017eb4938ce0ba38d1a5c40
mjalef <javadbabaei009@gmail.com>
2021-01-04 14:19:19 +0330

    init/icons: (FIX) fix interface and fill color / width and height

--- src/icons/admin/design/edit-alt.tsx
+++ src/icons/admin/design/edit-alt.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import { IIcon } from './interfaces';
+import { IIcon } from '../interfaces';
 
-export const ICEditAlt: FC<IIcon> = ({ className }) => {
+export const ICEditAlt: FC<IIcon> = ({ className,height,width,fill }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width ? width : 24}
+      height={height ? height : 24}
       viewBox="0 0 24 24"
       className={className}
     >
@@ -17,7 +17,7 @@ export const ICEditAlt: FC<IIcon> = ({ className }) => {
           transform="translate(-.498) translate(.498)"
         />
         <path
-          fill="current"
+          fill={fill ? fill : 'white'}
           d="M2.5 10.56l9 5.2a1 1 0 0 0 1 0l9-5.2a1 1 0 0 0 0-1.73l-9-5.2a1 1 0 0 0-1 0l-9 5.2a1 1 0 0 0 0 1.73zM12 5.65l7 4-7 4.05-7-4.01zm8.5 7.79L12 18.35l-8.5-4.91a1 1 0 1 0-1 1.73l9 5.2a1 1 0 0 0 1 0l9-5.2a1 1 0 1 0-1-1.73z"
           transform="translate(-.498) translate(.004 -.496)"
         />
--- src/icons/admin/design/menu.tsx
+++ src/icons/admin/design/menu.tsx
@@ -1,14 +1,14 @@
 import { FC } from 'react';
-import { IIcon } from './interfaces';
+import { IIcon } from '../interfaces';
 
-export const ICMenu: FC<IIcon> = ({ className }) => {
+export const ICMenu: FC<IIcon> = ({ className,height,width,fill }) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width={width ? width : 24}
+      height={height ? height : 24}
       viewBox="0 0 24 24"
-      className={className}
+      className={className} 
     >
       <g>
         <path
@@ -17,7 +17,7 @@ export const ICMenu: FC<IIcon> = ({ className }) => {
           transform="translate(.09) translate(-.09)"
         />
         <path
-          fill="#fff"
+          fill={fill ? fill : 'white'}
           d="M3 8h18a1 1 0 0 0 0-2H3a1 1 0 0 0 0 2zm18 8H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2zm0-5H3a1 1 0 0 0 0 2h18a1 1 0 0 0 0-2z"
           transform="translate(.09) translate(-.091 -.143)"
         />
