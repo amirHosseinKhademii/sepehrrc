ecfeb3c1f298d42273952cc319cad775f9f35a69
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-01-08 16:57:10 +0330

    init/layout: (FEAT) Currect use design function wording

--- src/hooks/use-design.ts
+++ src/hooks/use-design.ts
@@ -1,8 +1,10 @@
 import { DesignContext, designTypes } from 'providers/design-provider';
 import { useContext } from 'react';
+import { useService } from 'hooks';
 
 export const useDesign = () => {
   const { designDispatch, designState } = useContext(DesignContext);
+  const { upload } = useService();
 
   return {
     onInject: (arr, dragResult) => {
@@ -29,8 +31,8 @@ export const useDesign = () => {
           payload: dropResult,
         });
     },
-    onVerticalDrop: (dr) => {
-      designDispatch({ type: designTypes.ON_VERTICAL_DROP, payload: dr });
+    onVerticalDrop: (payload) => {
+      designDispatch({ type: designTypes.ON_VERTICAL_DROP, payload });
     },
     onDeleteItem: (payload) => {
       designDispatch({ type: designTypes.ON_DELETE_ITEM, payload });
@@ -38,22 +40,23 @@ export const useDesign = () => {
     setChildPayload: (index, items) => {
       return items.filter((_item, i) => i === index)[0];
     },
-    onSetItemSetting: (payload) => {
+    setSetting: (payload) => {
       designDispatch({
         type: designTypes.ON_SET_ITEM_SETTING,
         payload,
       });
     },
-    onSetItemProps: (payload) => {
+    setProps: (payload) => {
       designDispatch({
         type: designTypes.ON_SET_ITEM_PROPS,
         payload,
       });
     },
-    onSetItemImages: (payload) => {
+    setImage: async (file, number) => {
+      const result = await upload(file);
       designDispatch({
         type: designTypes.ON_SET_ITEM_IMAGES,
-        payload,
+        payload: { value: result.data.secure_url, number },
       });
     },
     designState,
