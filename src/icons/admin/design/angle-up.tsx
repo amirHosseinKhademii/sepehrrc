fec746fa868ca40b0e2a1fdfa88f1ef470aec3d9
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-01-10 17:51:07 +0330

    FT-7: (FIX) Fix drop down arrow functionality

---
+++ src/icons/admin/design/angle-up.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const ICAngelUp = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <path fill="none" d="M0 0H24V24H0z" />
+      <path
+        fill="#9ba3b5"
+        d="M17 14.835a1 1 0 0 1-1.41 0L12 11.3l-3.54 3.54a1 1 0 1 1-1.41-1.42l4.24-4.24a1 1 0 0 1 1.42 0L17 13.415a1 1 0 0 1 0 1.42z"
+        transform="translate(.119 -.555)"
+      />
+    </svg>
+  );
+};
