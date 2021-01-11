273c7b9c0fc299e1813287718fdeeb293a199211
mjalef <javadbabaei009@gmail.com>
2021-01-11 18:08:02 +0330

    fix bug on checkbox

--- src/components/admin/core/checkbox/CheckBox.tsx
+++ src/components/admin/core/checkbox/CheckBox.tsx
@@ -7,7 +7,10 @@ export const CheckBox: FC<ICheckbox> = ({ className, label }) => {
   return (
     <div className={join('flex items-center justify-end', className)}>
       <span className="text-14px text-gray_shade-300 mr-2">{label}</span>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        className="form-tick h-18px w-18px border border-gray_shade-500 rounded-md checked:bg-blue-500 checked:border-transparent focus:outline-none"
+      />
     </div>
   );
 };
