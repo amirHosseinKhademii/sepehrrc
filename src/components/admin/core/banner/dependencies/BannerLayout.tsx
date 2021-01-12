4de00dff76c7a0d3712da84a5bc50408dbd21d16
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-01-12 18:19:18 +0330

    FT-17: (FIX) Fix current banner shadow

--- src/components/admin/core/banner/dependencies/BannerLayout.tsx
+++ src/components/admin/core/banner/dependencies/BannerLayout.tsx
@@ -1,28 +1,27 @@
-import { useClass } from 'hooks';
-import { useUi } from 'hooks';
+import { useClass, useDesign } from 'hooks';
 
 export const BannerLayout = ({ children, className, onClick }) => {
-  const { uiState } = useUi();
-  const { join } = useClass();
+  const { join, toggle } = useClass();
+  const { designState } = useDesign();
 
   return (
     <div
-      className={`${
-        uiState.drawer.style
-          ? 'relative pt-12 pb-8 border-t-2 border-b-2 border-dashed border-opacity-70 border-primary-700'
-          : 'pt-12 cursor-pointer'
-      }`}
-      onClick={onClick}
+      className={toggle(
+        'pt-12',
+        'relative pt-12 pb-8 border-t-2 border-b-2 border-dashed border-opacity-70 border-primary-700',
+        designState.current.type == 'banner'
+      )}
+      // onClick={onClick}
     >
       <div className={join('grid w-5/6 h-full mx-auto gap-x-20px', className)}>
         {children}
       </div>
       <div
-        className={`${
-          uiState.drawer.style
-            ? 'absolute top-0 left-0 opacity-10 w-full h-full bg-primary-700'
-            : 'hidden'
-        }`}
+        className={toggle(
+          'hidden',
+          'absolute top-0 left-0 opacity-10 w-full h-full bg-primary-700',
+          designState.current.type == 'banner'
+        )}
       ></div>
     </div>
   );
