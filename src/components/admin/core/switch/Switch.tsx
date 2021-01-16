2c4b30eea6b91bbeb8200fbcb91d8d7615ea02e6
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-01-16 17:20:58 +0330

    FT-28: (FEAT) Implement slider dashboard initial logic

--- src/components/admin/core/switch/Switch.tsx
+++ src/components/admin/core/switch/Switch.tsx
@@ -2,20 +2,21 @@ import { useClass } from 'hooks';
 import { FC } from 'react';
 import { Text } from 'components';
 
-export const Switch: FC<ISwitch> = ({ className, label, checked }) => {
+export const Switch: FC<ISwitch> = ({ className, label, checked, onClick }) => {
   const { join } = useClass();
   return (
     <div
       className={join(
-        'flex flex-row justify-between items-center w-full',
+        'flex flex-row justify-between items-center w-full cursor-pointer',
         className
       )}
+      onClick={onClick}
     >
       <div className=" flex flex-row items-center justify-between w-52px h-32px p-2px bg-gray_shade-800 rounded-full cursor-pointer">
         {checked ? (
           <>
-            <div className="w-28px h-28px rounded-full bg-gray_shade-900"></div>
-            <div></div>
+            <div className="w-28px h-28px rounded-full bg-primary-700"></div>
+            <div />
           </>
         ) : (
           <>
