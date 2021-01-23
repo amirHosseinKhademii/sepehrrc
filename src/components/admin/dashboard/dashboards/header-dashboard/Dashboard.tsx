955e1c324ad522efbd0f728637fb5d77a9e651d8
mohammadmz <mrmz.manutd@gmail.com>
2021-01-23 17:27:38 +0330

    FT-55:(Refactor): headear dashboard redesigned

--- src/components/admin/dashboard/dashboards/header-dashboard/Dashboard.tsx
+++ src/components/admin/dashboard/dashboards/header-dashboard/Dashboard.tsx
@@ -1,17 +1,8 @@
 import { FC } from 'react';
 import { DrawerLayout } from 'components/admin/layouts';
 import { useClass, useDesign } from 'hooks';
-import {
-  Text,
-  Input,
-  CheckBox,
-  ButtonDrawer,
-  StyleBoxHeader,
-  HeaderDrawer,
-  ButtonGroupDrawer,
-} from 'components';
-import { MenuEditGroup } from './dependencies';
-import { ProductPrice } from 'components/admin/page/product/dependencies';
+import { StyleBoxHeader, HeaderDrawer, ButtonGroupDrawer } from 'components';
+import { MenuEditGroup, LogoUploader, InputBox } from './dependencies';
 
 export const HeaderDashboard = () => {
   const { join } = useClass();
@@ -20,120 +11,19 @@ export const HeaderDashboard = () => {
 
   const BaseSetttings = () => {
     return (
-      <>
+      <div className="w-full flex flex-col items-end pt-30px px-20px">
+        <StyleBoxHeader />
         <MenuEditGroup />
-        <StyleParts />
-      </>
-    );
-  };
-
-  const ButtonBox: FC<IBannerDashboard> = ({ label, number, className }) => {
-    return (
-      <div className={join('w-full  mb-30px ', className)}>
-        <Text className=" mb-14px text-14px text-white_shade-100 text-right">
-          {label}
-        </Text>
-        <ButtonDrawer
-          withUpload
-          text="انتخاب لوگو"
-          onUpload={(value) => setPureImage({ value, number })}
-        />
-        <Input
-          withLink
-          variant="inputIcon"
-          className="mt-15px"
-          placeholder={pureImage.number == number ? pureImage.link : ''}
-          onBlur={(event) => {
-            setPureImage({ number, link: event.target.value });
-          }}
-        />
-        <CheckBox
-          className="mt-15px"
-          label="باز کردن صفحه در تب جدید "
-          onClick={() => {
-            setPureImage({
-              number,
-              newTab: pureImage.newTab ? false : true,
-            });
-          }}
-          checked={pureImage.number == number && pureImage.newTab}
+        <LogoUploader />
+        <InputBox label="شماره تلفن" type="tel" placeholder="0519876543" />
+        <InputBox
+          label="دکمه هدر"
+          type="button"
+          placeholder="محصولات فروشگاه"
         />
       </div>
     );
   };
-
-  const InputBox = ({
-    label,
-    placeholder,
-    className,
-    type,
-  }: {
-    className?: string;
-    placeholder: string;
-    label: string;
-    type: string;
-  }) => {
-    const currentStyle = designState.current.settings.style;
-
-    if (
-      type === 'tel' &&
-      currentStyle !== 'first' &&
-      currentStyle !== 'second' &&
-      currentStyle !== 'fourth' &&
-      currentStyle
-    ) {
-      return (
-        <div className={join('w-full  mb-30px ', className)}>
-          <Text className=" mb-14px text-14px text-white_shade-100 text-right">
-            {label}
-          </Text>
-
-          <Input
-            placeholder="910000000"
-            maxLength={15}
-            variant="input"
-            withNumber
-            onBlur={(event) =>
-              setProps({ key: 'telNumber', value: event.target.value })
-            }
-          />
-        </div>
-      );
-    } else if (
-      type === 'button' &&
-      currentStyle !== 'second' &&
-      currentStyle !== 'seventh'
-    ) {
-      return (
-        <div className={join('w-full  mb-30px ', className)}>
-          <Text className=" mb-14px text-14px text-white_shade-100 text-right">
-            {label}
-          </Text>
-
-          <Input
-            maxLength={30}
-            placeholder="وروود یا عضویت"
-            variant="input"
-            onBlur={(event) =>
-              setProps({ key: 'buttonText', value: event.target.value })
-            }
-          />
-        </div>
-      );
-    } else {
-      return null;
-    }
-  };
-
-  const StyleParts = () => (
-    <div className="w-full flex flex-col items-end pt-30px px-20px">
-      <StyleBoxHeader />
-      <ButtonBox label="لوگو" number="one" />
-      <InputBox label="شماره تلفن" type="tel" placeholder="0519876543" />
-      <InputBox label="دکمه هدر" type="button" placeholder="محصولات فروشگاه" />
-    </div>
-  );
-
   return (
     <DrawerLayout>
       <HeaderDrawer setting text="تنظیمات هدر" />
