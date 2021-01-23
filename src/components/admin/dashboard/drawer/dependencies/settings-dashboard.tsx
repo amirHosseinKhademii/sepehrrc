d257c591c55e162c68dd405599faf561d1e5cb03
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-01-23 19:28:44 +0330

    FT-66:(FEAT) Implement drop down for setting dashboard

---
+++ src/components/admin/dashboard/drawer/dependencies/settings-dashboard.tsx
@@ -0,0 +1,60 @@
+import { DrawerLayout } from 'components/admin/layouts';
+import { HeaderDrawer } from '.';
+import { ButtonGroupDrawer, Button, DropDown } from 'components';
+
+export const SettingsDashboard = () => {
+  const ColorsButtons = () => (
+    <div className="flex flex-col px-20px pt-30px">
+      <p className="text-right text-white_shade-100 pt-15px">رنگ ها</p>
+      <div className="flex justify-between mt-20px">
+        <div className="flex flex-col items-center">
+          <Button className="h-50px bg-alert-900 mr-10px text-14px w-130px ">
+            #de4437
+          </Button>
+          <p className="text-gray_shade-300 pt-8px text-14px">رنگ دوم</p>
+        </div>
+        <div className="flex flex-col items-center">
+          <Button className="h-50px bg-alert-300 text-14px w-130px">
+            #de4437
+          </Button>
+          <p className="text-gray_shade-300 pt-8px text-14px">رنگ اصلی</p>
+        </div>
+      </div>
+    </div>
+  );
+
+  const FontDropDowns = () => (
+    <>
+      <div className="flex flex-col px-20px pt-30px">
+        <p className="text-right text-white_shade-100 pt-15px text-14px">
+          انتخاب فونت تیتر ها
+        </p>
+        <div className="felx flex-col mt-20px">
+          <DropDown
+            options={[{ id: 'yekan', title: 'یکان یخ' }]}
+            className="h-58px"
+          />
+        </div>
+      </div>
+      <div className="flex flex-col px-20px pt-30px">
+        <p className="text-right text-white_shade-100 pt-15px text-14px">
+          انتخاب فونت نوشته ها
+        </p>
+        <div className="felx flex-col mt-20px">
+          <DropDown
+            options={[{ id: 'yekan', title: 'یکان یخ' }]}
+            className="h-58px"
+          />
+        </div>
+      </div>
+    </>
+  );
+  return (
+    <DrawerLayout>
+      <HeaderDrawer setting />
+      <ColorsButtons />
+      <FontDropDowns />
+      <ButtonGroupDrawer />
+    </DrawerLayout>
+  );
+};
