ec24b3f4cc66e37ecf3c235c76efd5768f8ae968
Amirhoosein khademi <sourena2575@gmail.com>
2021-02-04 16:42:18 +0330

    FT-123: Implement backend services for get save and edit

--- src/hooks/use-service/use-service.ts
+++ src/hooks/use-service/use-service.ts
@@ -1,8 +1,31 @@
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { repository } from './repository';
+import axios from 'axios';
 
 const useService = () => {
   return {
+    onGet: (props: { url: string; onSuccess?: any; options?: any }) => {
+      const { url, onSuccess, options } = props;
+      const getter = async () => await axios.get(url);
+      return useQuery(`${url}`, getter, {
+        onSettled: onSuccess,
+        ...(options && options.enabled && { enabled: options.enabled }),
+      });
+    },
+    onPost: (props: { url: string; onSuccess?: any }) => {
+      const { url, onSuccess } = props;
+      const poster = async (model) => await axios.post(url, model);
+      return useMutation(poster, {
+        onSettled: onSuccess,
+      });
+    },
+    onPut: (props: { url: string; onSuccess?: any }) => {
+      const { url, onSuccess } = props;
+      const putter = async (model) => await axios.put(url, model);
+      return useMutation(putter, {
+        onSettled: onSuccess,
+      });
+    },
     upload: (file) => {
       const data = new FormData();
       data.append('file', file);
