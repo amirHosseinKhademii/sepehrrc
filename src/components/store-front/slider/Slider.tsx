72eb86d701315cf3eee62c90968f280eaf851088
mjalef <javadbabaei009@gmail.com>
2021-02-06 14:30:35 +0330

    FT-116 : (FIX) slider bug

--- src/components/store-front/slider/Slider.tsx
+++ src/components/store-front/slider/Slider.tsx
@@ -1,5 +1,7 @@
-import { Fragment } from 'react';
+import { Fragment, useRef } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { ICAngelLeft, ICAngelRight } from 'icons';
+import AliceCarousel from 'react-alice-carousel';
 import SwiperCore, {
   Navigation,
   Pagination,
@@ -23,45 +25,94 @@ SwiperCore.use([
 ]);
 
 export const Slider = ({ child, speed, screen, button, effect }) => {
-  return (
-    <Fragment>
-      <Swiper
-        effect={effect}
-        spaceBetween={30}
-        speed={speed}
-        pagination={{
-          el: '.swiper-paginations',
-          type: 'bullets',
-          clickable: true,
-        }}
-        navigation={{
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        }}
-        autoplay={{ delay: 200, disableOnInteraction: false }}
-        className={`h-450px w-full rounded`}
-      >
-        {child.length > 0 ? (
-          child
-        ) : (
-          <SwiperSlide className="bg-gray_shade-200 originalSlider flex justify-center items-center">
-            <span className="text-2xl text-gray-700">اسلایدر</span>
-          </SwiperSlide>
-        )}
-        <div
-          className={`swiper-button-next ${button === 'first' ? 'hidden' : ''}`}
-        ></div>
-        <div
-          className={`swiper-button-prev ${button === 'first' ? 'hidden' : ''}`}
-        ></div>
-      </Swiper>
+  const sliderRef = useRef(null);
+
+  const renderDotsItem = ({ isActive }) => {
+    return isActive ? (
+      <div className="flex h-8px w-8px mx-1 rounded-full bg-gray_shade-800 -m-6"></div>
+    ) : (
+      <div className="flex h-8px w-8px mx-1 rounded-full bg-gray_shade-600 -m-6 cursor-pointer"></div>
+    );
+  };
+  const renderPrevButton = ({ isDisabled }) => {
+    return (
       <div
-        className={`swiper-paginations ${
-          button === 'second'
-            ? 'hidden'
-            : 'absolute inset-x-0 mx-auto flex justify-center items-center h-4 focus:outline-none'
-        }`}
-      ></div>
-    </Fragment>
+        className="absolute top-0 left-0 h-450px text-white flex justify-center items-center cursor-pointer "
+        style={{ opacity: isDisabled ? '0.5' : 1 }}
+      >
+        <ICAngelLeft fill="white" />
+      </div>
+    );
+  };
+
+  const renderNextButton = ({ isDisabled }) => {
+    return (
+      <span
+        className="absolute top-0 right-0 h-450px text-white flex justify-center items-center cursor-pointer "
+        style={{ opacity: isDisabled ? '0.5' : 1 }}
+      >
+        <ICAngelRight fill="white" />
+      </span>
+    );
+  };
+  return (
+    <div className="relative">
+      <AliceCarousel
+        autoPlay
+        autoPlayStrategy="default"
+        autoPlayInterval={speed}
+        animationDuration={1000}
+        animationType={effect}
+        infinite
+        touchTracking={false}
+        mouseTracking={false}
+        renderDotsItem={renderDotsItem}
+        renderPrevButton={renderPrevButton}
+        renderNextButton={renderNextButton}
+        items={child}
+        ref={sliderRef}
+        disableDotsControls={button === 'second' ? true : false}
+        disableButtonsControls={button === 'first' ? true : false}
+      />
+    </div>
+
+    // <Swiper
+    //   slidesPerView={1}
+    //   effect={effect}
+    //   speed={speed}
+    //   loop
+    //   pagination={{
+    //     el: '.swiper-paginations',
+    //     type: 'bullets',
+    //     clickable: true,
+    //   }}
+    //   navigation={{
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    //   }}
+    //   autoplay={{ delay: 200 }}
+    //   className={`h-450px w-full rounded`}
+    // >
+    //   {child.length > 0 ? (
+    //     child
+    //   ) : (
+    //     <SwiperSlide className="bg-gray_shade-200 originalSlider flex justify-center items-center">
+    //       <span className="text-2xl text-gray-700">اسلایدر</span>
+    //     </SwiperSlide>
+    //   )}
+    //   <div
+    //     className={`swiper-button-next ${button === 'first' ? 'hidden' : ''}`}
+    //   ></div>
+    //   <div
+    //     className={`swiper-button-prev ${button === 'first' ? 'hidden' : ''}`}
+    //   ></div>
+    // </Swiper>
+    // <div
+    //   className={`swiper-paginations ${
+    //     button === 'second'
+    //       ? 'hidden'
+    //       : 'absolute inset-x-0 mx-auto flex justify-center items-center h-4 focus:outline-none'
+    //   }`}
+    // ></div>
   );
 };
--- src/icons/admin/design/angle-left.tsx
+++ src/icons/admin/design/angle-left.tsx
@@ -1,22 +1,27 @@
-import * as React from 'react';
+import { FC } from 'react';
 
-function SvgAngleLeft(props: React.SVGProps<SVGSVGElement>) {
+export const ICAngelLeft: FC<IIcon> = ({
+  className,
+  onClick,
+  height,
+  width,
+  fill,
+}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="1em"
-      height="1em"
+      width={width ? width : 58}
+      height={height ? height : 58}
       viewBox="0 0 24 24"
-      {...props}
+      className={className}
+      onClick={onClick}
     >
       <path data-name="Rectangle 29" fill="none" d="M0 0h24v24H0z" />
       <path
         data-name="angle-left"
         d="M11.913 12.675l3.54-3.54a1 1 0 10-1.42-1.41l-4.24 4.24a1 1 0 000 1.42l4.24 4.29a1 1 0 101.42-1.41z"
-        fill='current'
+        fill={fill ? fill : 'current'}
       />
     </svg>
   );
-}
-
-export default SvgAngleLeft;
+};
