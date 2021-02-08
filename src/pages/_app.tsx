5ab0afddd8022c2929c74336b2b32e6b7e8e7fd9
mohammadmz <mrmz.manutd@gmail.com>
2021-02-08 13:24:34 +0330

    (FT-129):replace new carousel n product and brand

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,19 +1,10 @@
 import '../styles/index.css';
+import '../styles/style.css';
 import { UiProvider } from 'providers/ui-provider';
 import { DesignProvider } from 'providers/design-provider';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Head from 'next/head';
 import { Fragment } from 'react';
-import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
-import 'swiper/components/pagination/pagination.scss';
-import 'swiper/components/scrollbar/scrollbar.scss';
-import 'swiper/components/effect-fade/effect-fade.scss';
-import 'swiper/components/effect-flip/effect-flip.scss';
-import 'swiper/components/effect-coverflow/effect-coverflow.scss';
-import 'swiper/components/a11y/a11y.scss';
-import 'swiper/components/controller/controller.scss';
-
 import 'react-alice-carousel/lib/alice-carousel.css';
 
 function MyApp({ Component, pageProps }) {
