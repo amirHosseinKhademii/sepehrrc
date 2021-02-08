c23e18e509acd42e0792a36f3455e0d95155842a
mjalef <javadbabaei009@gmail.com>
2021-02-08 18:00:50 +0330

    FT-113(FIX):Fix footer style issue on rtl direction

--- src/containers/store-front/footer/FooterContainer.tsx
+++ src/containers/store-front/footer/FooterContainer.tsx
@@ -1,7 +1,7 @@
 import { Footer } from 'components';
 export const DesignFooter = () => {
   return (
-    <div className="absolute bottom-0 w-full bg-gray-200 h-20">
+    <div className="absolute bottom-0  w-full bg-gray-200 h-20">
       <Footer />
     </div>
   );
