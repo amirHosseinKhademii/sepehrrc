fd4a335e2ffe678a6d6d9c5c1b381df4ba16cfb8
AmirHossein Khademi <amirhossein.khademi@rdsysco.com>
2021-02-16 18:15:44 +0330

    FT-155: Fix provider

--- src/components/design/dashboards/settings-dashboard/dependencies/language-drop.tsx
+++ src/components/design/dashboards/settings-dashboard/dependencies/language-drop.tsx
@@ -1,15 +1,16 @@
 import { DropDown, Text } from 'components';
-import { useDesign } from 'hooks';
+import { useDesign, useDirection } from 'hooks';
 export const LanguageDrop = () => {
   const { designState, onPageSetting } = useDesign();
+  const { textAlignRtl } = useDirection();
 
   return (
     <div className="flex flex-col mt-25px  px-20px">
-      <Text className="mb-15px text-14px text-white_shade-100 text-right">
-        اتخاب زبان
-      </Text>
+      <p className={`${textAlignRtl}  text-white_shade-100 pt-15px text-14px`}>
+        انتخاب زبان
+      </p>
       <DropDown
-        className="w-full h-54px "
+        className="w-full h-54px mt-20px "
         options={[
           { id: 'rtl', title: `فارسی` },
           { id: 'ltr', title: `English` },
--- src/hooks/use-language/use-language.ts
+++ src/hooks/use-language/use-language.ts
@@ -3,6 +3,7 @@ import { en, fa } from 'languages';
 export const useLanguage = () => {
   const { designState } = useDesign();
   const { language } = designState.pageSettings;
+
   return {
     languageText: language === 'farsi' ? fa : en,
   };
