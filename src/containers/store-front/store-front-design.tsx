52f1df32e386596a09213ba7d9540b6b5d435656
mjalef <javadbabaei009@gmail.com>
2021-02-17 18:06:41 +0330

    fixFT-171:(FIX) fix bug on footer

--- src/containers/store-front/store-front-design.tsx
+++ src/containers/store-front/store-front-design.tsx
@@ -16,33 +16,35 @@ export const StoreFrontDesignContainer = memo(() => {
 
   return (
     <Fragment>
-      {/* <HeaderContainer /> */}
-      <Container
-        groupName="ADMIN_DESIGN"
-        dragClass="bg-red-600"
-        onDrop={onHorizontalDrop(drop)}
-        getChildPayload={(index) => setChildPayload(index, designState.page)}
-        onDragEnd={(e) => (e !== null ? setDrop(e) : {})}
-        style={{
-          width: '100%',
-          minHeight: '60vh',
-        }}
-        dropPlaceholder={{
-          animationDuration: 150,
-          showOnTop: true,
-          className: 'drop-preview',
-        }}
-      >
-        {designState.pageItems.map((item, index) => (
-          <Fragment key={index}>
-            {item.type == 'products' && <ProductContainer item={item} />}
-            {item.type == 'slider' && <SliderContainer item={item} />}
-            {item.type == 'banner' && <BannerContainer item={item} />}
-            {item.type == 'brands' && <BrandContainer item={item} />}
-          </Fragment>
-        ))}
-      </Container>
-      {/* <FooterContainer /> */}
+      <HeaderContainer />
+      <div className=" w-full h-auto flex-grow relative pb-28 bg-gray_tint-16">
+        <Container
+          groupName="ADMIN_DESIGN"
+          dragClass="bg-red-600"
+          onDrop={onHorizontalDrop(drop)}
+          getChildPayload={(index) => setChildPayload(index, designState.page)}
+          onDragEnd={(e) => (e !== null ? setDrop(e) : {})}
+          style={{
+            width: '100%',
+            minHeight: '60vh',
+          }}
+          dropPlaceholder={{
+            animationDuration: 150,
+            showOnTop: true,
+            className: 'drop-preview',
+          }}
+        >
+          {designState.pageItems.map((item, index) => (
+            <Fragment key={index}>
+              {item.type == 'products' && <ProductContainer item={item} />}
+              {item.type == 'slider' && <SliderContainer item={item} />}
+              {item.type == 'banner' && <BannerContainer item={item} />}
+              {item.type == 'brands' && <BrandContainer item={item} />}
+            </Fragment>
+          ))}
+        </Container>
+      </div>
+      <FooterContainer />
     </Fragment>
   );
 });
