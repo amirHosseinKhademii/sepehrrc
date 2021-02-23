b431708d242a8c0864108570afad88cacb46310c
Amirhoosein khademi <sourena2575@gmail.com>
2021-02-23 19:18:26 +0330

    FT-193: Fix footer dashboard

--- src/components/design/dashboards/footer-dashboard/dependencies/style-box-footer/dependencies/drop-down.tsx
+++ src/components/design/dashboards/footer-dashboard/dependencies/style-box-footer/dependencies/drop-down.tsx
@@ -1,6 +1,8 @@
 import { LabelBox } from '.';
 import FooterFirst from './footer-first';
 import FooterSecond from './footer-second';
+import FooterFourth from './footer-fourth';
+import FooterThird from './footer-third';
 
 const DropDown = ({ designState, onSelectClick, join }) => {
   const { style } = designState.current.settings;
@@ -18,6 +20,16 @@ const DropDown = ({ designState, onSelectClick, join }) => {
         onClick={() => onSelectClick({ style: 'second' })}
         join={join}
         active={style === 'second'}
+      />
+      <LabelBox label="استایل 3" />
+      <FooterThird
+        onClick={() => onSelectClick({ style: 'third' })}
+        active={style === 'third'}
+      />
+      <LabelBox label="استایل 4" />
+      <FooterFourth
+        onClick={() => onSelectClick({ style: 'fourth' })}
+        active={style === 'fourth'}
       />
     </div>
   );
