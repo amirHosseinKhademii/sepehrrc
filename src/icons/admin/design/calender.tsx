5bb11109ce918881b28fd10ac1975e95d205b62d
Amirhoosein khademi <sourena2575@gmail.com>
2021-03-02 19:27:05 +0330

    deploy:fix issue

--- src/icons/admin/design/calender.tsx
+++ src/icons/admin/design/calender.tsx
@@ -22,7 +22,6 @@ export const ICCalender: FC<IIcon> = ({
       <path
         data-name="calender"
         d="M19.74 3.57h-2v-1a1 1 0 00-2 0v1h-6v-1a1 1 0 00-2 0v1h-2a3 3 0 00-3 3v12a3 3 0 003 3h14a3 3 0 003-3v-12a3 3 0 00-3-3zm1 15a1 1 0 01-1 1h-14a1 1 0 01-1-1v-7h16zm0-9h-16v-3a1 1 0 011-1h2v1a1 1 0 002 0v-1h6v1a1 1 0 002 0v-1h2a1 1 0 011 1z"
-        fill="current"
         fill={fill ? fill : 'current'}
       />
     </svg>
