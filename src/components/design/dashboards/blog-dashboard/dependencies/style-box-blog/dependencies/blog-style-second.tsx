f396c23a7cedf0f2512c50352f1f8829f6e896de
mjalef <javadbabaei009@gmail.com>
2021-03-02 12:36:24 +0330

    fix bug

--- src/components/design/dashboards/blog-dashboard/dependencies/style-box-blog/dependencies/blog-style-second.tsx
+++ src/components/design/dashboards/blog-dashboard/dependencies/style-box-blog/dependencies/blog-style-second.tsx
@@ -4,7 +4,6 @@ import { ICBlogStyleSecond } from 'icons';
 const BlogStyleSecond: FC<IStyleBox> = memo(
   ({ className, onClick, active = false }) => {
     const { join } = useClass();
-    alert(active);
     return (
       <ICBlogStyleSecond
         fill={active ? '#1578e6' : '#2e323d'}
